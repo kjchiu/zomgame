fbb5a4a80cd14c49368e076e141e31596851869e
krypes <krypes@9b66597e-bb4a-0410-bce4-15c857dd0990>
2008-06-01 08:21:23 +0000

    fixed: zombies can now move on diagonals

--- zomgame/ZGame/huntingstate.cpp
+++ zomgame/ZGame/huntingstate.cpp
@@ -1,4 +1,5 @@
 #include "huntingstate.h"
+#include "globals.h"
 
 HuntingState::HuntingState(Zombie* zombie) : BrainState(zombie) {
 }
@@ -10,9 +11,16 @@ void HuntingState::tick(Game* game) {
 		} 
 	Coord delta = *game->getPlayer()->getLoc() - *zombie->getLoc();
 	Direction dx, dy;
-	dx = (delta.getX() > 0) ? ::EAST : ::WEST;
-	dy = (delta.getY() > 0) ? ::SOUTH : ::NORTH;
-	Direction moveDir = (abs(delta.getX()) > abs(delta.getY())) ? dx : dy;
+	dx = (delta.getX() > 0) ? EAST : WEST;
+	dy = (delta.getY() > 0) ? SOUTH : NORTH;
+	Direction moveDir;
+	if (dx && dy && ((dx / dy < 0.7) || (dx / dy > 1.3))) {
+		int offset = (dx - EAST) ? 1 : -1;
+		moveDir = (Direction)(offset + dy);
+	} else {
+		moveDir = (abs(delta.getX()) > abs(delta.getY())) ? dx : dy;
+	}
+
 	if (delta.getX() || delta.getY()) {
 		zombie->queueMove(game->getTime() + zombie->getSpeed(), moveDir);
 	}
