64ce82f9cad8a30d099491eb2ebb381b835e761a
nicholasbale <nicholasbale@9b66597e-bb4a-0410-bce4-15c857dd0990>
2008-06-10 06:02:08 +0000

    Re-Added PropFactory. Won't compile due to some weird-arsed linking error.

---
+++ zomgame/ZGame/propFactory.cpp
@@ -0,0 +1,25 @@
+#include "propFactory.h"
+
+Prop* PropFactory::createDoor(){
+	return createDoor(100);
+}
+
+Prop* PropFactory::createDoor(int durability){
+	Prop* door = new Door();
+	door->getDurability()->changeMaxValueBy(durability - 100);
+	door->getDurability()->changeCurValueBy(durability - 100);
+	return door;
+}
+
+Prop* PropFactory::createWall(){
+	return createWall(100);
+}
+
+Prop* PropFactory::createWall(int durability){
+	Prop* wall = new Prop(false);
+	wall->setName("Wall");
+	wall->getDurability()->changeMaxValueBy(durability - 100);
+	wall->getDurability()->changeCurValueBy(durability - 100);
+	return door;
+
+}
