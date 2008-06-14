0a8758a8d7961de2b2c3d41019747df154fe4a8c
nicholasbale <nicholasbale@9b66597e-bb4a-0410-bce4-15c857dd0990>
2008-06-14 06:31:09 +0000

    Mapblock info displays in the menuWin.

--- trunk/zomgame/ZGame/display.h
+++ trunk/zomgame/ZGame/display.h
@@ -69,7 +69,7 @@ class Display {
 		void draw(Inventory* inventory);
 		void draw(Map* map);	
 		void draw(deque<Message> msgs);
-		void draw(Player* player);
+		void draw(Player* player, MapBlock* block);
 		void drawInventoryList(vector<Item*> items, int yLoc, int selection, bool highlight);
 		void drawItemDetails(Item* item, int height, int width);
 		void drawPopup(Item* item);	//draw the skills an item can utilize 
