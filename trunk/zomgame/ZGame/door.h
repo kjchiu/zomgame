3d7ea871f97be52a72d2c037ce9d7d3f144b968d
krypes <krypes@9b66597e-bb4a-0410-bce4-15c857dd0990>
2008-06-16 04:19:57 +0000

    moved door implementation to door.cpp damnit nick

--- trunk/zomgame/ZGame/door.h
+++ trunk/zomgame/ZGame/door.h
@@ -8,23 +8,8 @@
 class Door : public Prop {
 	public:
 		Door() {setName("Door"); setDisplayChar(0x2666); setPassable(false);}
-		int interact(Player* p) {
-			if (isPassable()){
-				setDisplayChar(0x2666);
-				setPassable(false);
-				return 5;
-			} else {
-				setDisplayChar(' ');
-				setPassable(true);
-				return 0;
-			}
-		}
-
-		vector<Item*> destroy() {
-			vector<Item*> debris;
-			debris.push_back(ItemFactory::createWoodPlank());
-			return debris;
-		}
+		int interact(Player* p);
+		vector<Item*> destroy();
 };
 
 #endif DOOR_H
